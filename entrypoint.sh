aeb6df8280bb990c89bc6348a66c85b62597eede
Rahul Raval <rahulraval870@gmail.com>
2024-02-12 13:07:51 +0530

    entrypoint.sh file added

---
+++ entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+
+# Set environment variables with your Supabase URL and API key
+# export VITE_SUPABASE_URL="https://your-supabase-url.supabase.co"
+# export VITE_SUPABASE_API_KEY="your-supabase-api-key"
+
+#  Start your Vite app
+# npm run dev
+
+
+# Set environment variables with your Supabase URL and API key
+export VITE_SUPABASE_URL="https://hayrypeodyayikntcjxd.supabase.co"
+export VITE_SUPABASE_API_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhheXJ5cGVvZHlheWlrbnRjanhkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDU1Nzg4NTYsImV4cCI6MjAyMTE1NDg1Nn0.rRijQAyq7ZdOdYMYaBW8qcNucC6dyDHkhemKikHzUq0"
+
+# Start your Vite app
+npm run dev
+
+
